29b89f0c755781c7393a0c6f83a15298f47f77ce
enoch232 <enoch232@gmail.com>
2016-06-09 01:13:47 -0700

    fix problems, and add profile photo, and make everything functional

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,21 @@
 class Search < ApplicationRecord
+	def properties
+		@properties ||= search_posts
+
+	end
+
+	private
+	def search_posts
+		posts = Post.all
+		posts = posts.where(["title LIKE ?","%#{keyword}%"]) if keyword.present?
+		posts = posts.where(["category = ?", category]) if category.present?
+		posts = posts.where(["price >= ?", minprice]) if minprice.present?
+		posts = posts.where(["price < ?", maxprice]) if maxprice.present?
+		posts = posts.where(["state LIKE ?", state]) if state.present?
+		posts = posts.where(["school LIKE ?", school]) if school.present?
+		posts = posts.where(["condition = ?", condition]) if condition.present?
+		posts = posts.where(["specific_category LIKE ?", specific_category]) if specific_category.present?
+		posts
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606215453) do
+ActiveRecord::Schema.define(version: 20160609080944) do
 
   create_table "pictures", force: :cascade do |t|
     t.datetime "created_at",         null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160606215453) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -52,19 +53,23 @@ ActiveRecord::Schema.define(version: 20160606215453) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                     default: "", null: false
+    t.string   "encrypted_password",        default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",             default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "name"
+    t.string   "profilephoto_file_name"
+    t.string   "profilephoto_content_type"
+    t.integer  "profilephoto_file_size"
+    t.datetime "profilephoto_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
