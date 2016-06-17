b3a5d43bbeae53594b52b4dc5394b9d3d58d1eb0
enoch232 <enoch232@gmail.com>
2016-06-17 10:57:45 -0700

    fill methods for conversation controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,7 +3,26 @@ class ConversationsController < ApplicationController
   	@conversations = Conversation.all.where("sender=? OR receiver=?",current_user, current_user)
   end
   def new
+  	@conversation = Conversation.new
+  end
+  def create
+  	@conversation = Conversation.new(conversation_params)
+  	if @conversation.save
+  		respond_to do |format|
+  		  format.html{ redirect_to @conversation }
+  		  format.js{}
+  		end
+  	else
+  		render :new
+  	end
   end
   def destroy
+  end
+  private
+  def conversation_params
+  	params.require(:conversation).permit(:sender, :receiver)
+  end
+  def conversation_find
+  	@conversation = Conversation.find(params[:id])
   end
 end
