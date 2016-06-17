44fd5555144e6e0c332ac5776aa6d369c37876d5
enoch232 <enoch232@gmail.com>
2016-06-17 10:45:22 -0700

    fix weird bug

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,6 +1,6 @@
 class ConversationsController < ApplicationController
   def index
-  	
+  	@conversations = Conversation.all.where("sender=? OR receiver=?",current_user, current_user)
   end
   def new
   end
